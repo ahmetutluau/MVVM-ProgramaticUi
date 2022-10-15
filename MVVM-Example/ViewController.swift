9188091d6bc92ce19541d1007b307d3831147bfa
Ahmet Utlu <ahmet.utlu@id3.com.tr>
2022-10-15 14:09:49 -0700

    Initial Commit

---
+++ MVVM-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Example
+//
+//  Created by MAC on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
